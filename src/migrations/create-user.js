b6556ba0bc9a6514ff8b60e4a02741b7daccace0
phatnt472 <20521737@gm.uit.edu.vn>
2024-01-09 20:06:05 +0700

    third commit

--- src/migrations/create-user.js
+++ src/migrations/create-user.js
@@ -17,11 +17,13 @@ module.exports = {
       role_code: {type: Sequelize.STRING},
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     });
   },
